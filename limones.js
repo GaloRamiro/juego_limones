32a19a03883313c2abf90d1c1ca9b020f11497d2
GaloRamiro <g_cisneros_23@hotmail.es>
2026-04-09 14:14:05 -0500

    mover izquierda y derecha al personaje

--- limones.js
+++ limones.js
@@ -5,6 +5,8 @@ const ALTURA_SUELO = 20;
 const ALTURA_PERSONAJE =60;
 const ANCHO_PERSONAJE = 40;
 
+let personajeX = canvas.width/2;
+
 function dibujarSuelo(){
     ctx.fillStyle= "blue";
     ctx.fillRect(0,canvas.height - ALTURA_SUELO, canvas.width,ALTURA_SUELO);
@@ -12,11 +14,30 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle= "yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE ) , ANCHO_PERSONAJE,ALTURA_PERSONAJE );
+    ctx.fillRect(personajeX,canvas.height -(ALTURA_SUELO+ALTURA_PERSONAJE ) , ANCHO_PERSONAJE,ALTURA_PERSONAJE );
 }
 
 function inciar(){
     dibujarSuelo();
     dibujarPersonaje();
     
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+
+}
+
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla()
+}
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+function limpiarCanva(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
 }
